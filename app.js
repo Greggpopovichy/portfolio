1464c89edb32e087bc629c9c40e4489ecbf75203
Charlie Arrendale <carrendale@gmail.com>
2018-02-15 12:07:19 -0500

    added video section

---
+++ app.js
@@ -0,0 +1,85 @@
+$('#video1').on('click', function() {
+    $(this).html('<iframe width="560" height="315" src="https://www.youtube.com/embed/xisMTQGdGFw" frameborder="0" allowfullscreen></iframe>').css('background', 'none');
+});
+
+$('#video2').on('click', function() {
+    $(this).html('<iframe width="560" height="315" src="https://www.youtube.com/embed/FSfXFBNmZMs" frameborder="0" allowfullscreen></iframe>').css('background', 'none');
+});
+
+$('#video3').on('click', function() {
+    $(this).html('<iframe width="560" height="315" src="https://www.youtube.com/embed/HDrbOdMUyJE" frameborder="0" allowfullscreen></iframe>').css('background', 'none');
+});
+
+$('#video4').on('click', function() {
+    $(this).html('<iframe width="560" height="315" src="https://www.youtube.com/embed/iWJidKFuC9s" frameborder="0" allowfullscreen></iframe>').css('background', 'none');
+});
+
+$('#video5').on('click', function() {
+    $(this).html('<iframe width="560" height="315" src="https://www.youtube.com/embed/gKmhpYz5jcc" frameborder="0" allowfullscreen></iframe>').css('background', 'none');
+});
+
+$('#video6').on('click', function() {
+    $(this).html('<iframe width="560" height="315" src="https://www.youtube.com/embed/61SsSeiBzxw" frameborder="0" allowfullscreen></iframe>').css('background', 'none');
+});
+
+$('#video7').on('click', function() {
+    $(this).html('<iframe width="560" height="315" src="https://www.youtube.com/embed/7YP9CliPRLY" frameborder="0" allowfullscreen></iframe>').css('background', 'none');
+});
+
+$('#video8').on('click', function() {
+    $(this).html('<iframe width="560" height="315" src="https://www.youtube.com/embed/zgwZPeGRBmM" frameborder="0" allowfullscreen></iframe>').css('background', 'none');
+});
+
+$('#video9').on('click', function() {
+    $(this).html('<iframe width="560" height="315" src="https://www.youtube.com/embed/-YlphnLDev4" frameborder="0" allowfullscreen></iframe>').css('background', 'none');
+});
+
+$('#video10').on('click', function() {
+    $(this).html('<iframe width="560" height="315" src="https://www.youtube.com/embed/jrRK_B7DFyo" frameborder="0" allowfullscreen></iframe>').css('background', 'none');
+});
+
+$('#video11').on('click', function() {
+    $(this).html('<iframe width="560" height="315" src="https://www.youtube.com/embed/qem4Xf1qXTQ" frameborder="0" allowfullscreen></iframe>').css('background', 'none');
+});
+
+$('#video12').on('click', function() {
+    $(this).html('<iframe width="560" height="315" src="https://www.youtube.com/embed/7x4tRVRafyU" frameborder="0" allowfullscreen></iframe>').css('background', 'none');
+});
+
+$('#video13').on('click', function() {
+    $(this).html('<iframe width="560" height="315" src="https://www.youtube.com/embed/029ZwxIolgI" frameborder="0" allowfullscreen></iframe>').css('background', 'none');
+});
+
+$('#video14').on('click', function() {
+    $(this).html('<iframe width="560" height="315" src="https://www.youtube.com/embed/Jjox9Gq9HWg" frameborder="0" allowfullscreen></iframe>').css('background', 'none');
+});
+
+$('#video15').on('click', function() {
+    $(this).html('<iframe width="560" height="315" src="https://www.youtube.com/embed/PeO-3chN4rI" frameborder="0" allowfullscreen></iframe>').css('background', 'none');
+});
+
+$('#video16').on('click', function() {
+    $(this).html('<iframe width="560" height="315" src="https://www.youtube.com/embed/Qd1jvs3D8jk" frameborder="0" allowfullscreen></iframe>').css('background', 'none');
+});
+
+$('#video17').on('click', function() {
+    $(this).html('<iframe width="560" height="315" src="https://www.youtube.com/embed/vp8NUk7sgzs" frameborder="0" allowfullscreen></iframe>').css('background', 'none');
+});
+
+$('#video18').on('click', function() {
+    $(this).html('<iframe width="560" height="315" src="https://www.youtube.com/embed/3tbKMSDwsVM" frameborder="0" allowfullscreen></iframe>').css('background', 'none');
+});
+
+$('#video19').on('click', function() {
+    $(this).html('<iframe width="560" height="315" src="https://www.youtube.com/embed/i4ECBAvMFXE" frameborder="0" allowfullscreen></iframe>').css('background', 'none');
+});
+
+$('#video20').on('click', function() {
+    $(this).html('<iframe width="560" height="315" src="https://www.youtube.com/embed/PkOj_n322sY" frameborder="0" allowfullscreen></iframe>').css('background', 'none');
+});
+
+$('#video21').on('click', function() {
+    $(this).html('<iframe width="560" height="315" src="https://www.youtube.com/embed/YmgBuofixBI" frameborder="0" allowfullscreen></iframe>').css('background', 'none');
+});
+
+
